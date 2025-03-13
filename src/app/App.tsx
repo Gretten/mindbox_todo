fae6f3d0e59b72b7f2451427db11ffe0a705aee3
Denis Sherbakov <dsherbakov@rbc.ru>
2025-03-13 12:00:20 +0300

    feat: app layer

---
+++ src/app/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import styles from './global.module.css';
+
+function App() {
+
+  return (
+    <>
+      <div className={styles.app}>
+
+      </div>
+    </>
+  )
+}
+
+export default App
