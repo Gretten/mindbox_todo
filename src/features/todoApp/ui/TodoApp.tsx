1334972b1685204043d4d9d63b68688f3eeb23d5
Denis Sherbakov <dsherbakov@rbc.ru>
2025-03-14 17:52:56 +0300

    feat: control panel

--- src/features/todoApp/ui/TodoApp.tsx
+++ src/features/todoApp/ui/TodoApp.tsx
@@ -1,15 +1,20 @@
+import Button from "@mui/material/Button/Button";
 import { TodoAdd, TodoItem } from "../../../entities/todo";
 import { useTodoAppStore } from "../model/useTodoAppStore";
 import styles from "./style.module.css";
+import { MinorButton } from "../../../shared/ui/MinorButton";
+import ButtonGroup from "@mui/material/ButtonGroup/ButtonGroup";
 
 export const TodoApp = () => {
   const { todos, addTodo, deleteTodo, updateTodo } = useTodoAppStore();
 
   const hasTodos = todos.length > 0;
+  const activeTodosNumber =
+    todos.filter((todo) => !todo.isCompleted).length || 0;
 
   return (
     <div className={styles["app-container"]}>
-      <h1>Todo App</h1>
+      <h1>Продуктивник 🐝</h1>
       <div className="add-todo-form">
         <TodoAdd onAdd={addTodo} />
       </div>
@@ -24,9 +29,26 @@ export const TodoApp = () => {
             />
           ))}
 
-        {!hasTodos && <p>No todos</p>}
+        {!hasTodos && <p>Всё сделано! ☑</p>}
       </div>
-      <div className="control-panel"></div>
+      <div className={styles["control-panel"]}>
+        <div className={styles["control-panel-filter"]}>
+          <ButtonGroup>
+            <MinorButton variant="text" size="small">
+              Все
+            </MinorButton>
+            <MinorButton variant="text" size="small">
+              Готовые
+            </MinorButton>
+            <MinorButton variant="text" size="small">
+              Активные
+            </MinorButton>
+          </ButtonGroup>
+        </div>
+        <MinorButton variant="text" size="small">
+          Очистить
+        </MinorButton>
+      </div>
     </div>
   );
 };
---
+++ src/shared/ui/MinorButton.tsx
@@ -0,0 +1,7 @@
+import { styled } from "@mui/material/styles";
+import Button, { ButtonProps } from "@mui/material/Button/Button";
+
+export const MinorButton = styled(Button)<ButtonProps>({
+  fontSize: 12,
+  color: "gray",
+});
